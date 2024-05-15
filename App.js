8f2cd6229c52df16b6516dbb2a012fa7cc1182db
Patrik Nygren <patriknygren82@gmail.com>
2024-05-15 14:15:24 +0200

    added something...

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 
 import PrimaryButton from "./components/PrimaryButton";
 import StartGameScreen from "./screens/StartGameScreen";
+import Colors from "./constants/Colors";
 
 export default function App() {
   const [userChoice, setUserChoice] = useState(null); // valt nummer
@@ -12,24 +14,45 @@ export default function App() {
 
   const chooseNumberHandler = (number) => {
     setUserChoice(number);
-    console.log(number);
   };
 
   let screen = <StartGameScreen onChooseNumber={chooseNumberHandler} />;
 
+  if (userChoice) {
+    screen = (
+      <GameScreen
+        userChoice={userChoice}
+        onGuess={() => setRounds(rounds + 1)}
+        onGameOver={() => setGameOver(true)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      {screen}
-    </View>
+    <LinearGradient
+      style={styles.container}
+      colors={[Colors.primary700, Colors.accent500]}
+    >
+      <ImageBackground
+        source={require("./assets/background.png")}
+        style={styles.image}
+        imageStyle={styles.backgroundImage}
+        resizeMode="cover"
+      >
+        {screen}
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.25,
   },
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,3 +1,32 @@
-const GameScreen = () => {};
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import Title from "../components/Title";
+
+function generateRandomNumber(min, max, exclude) {
+  const number = Math.floor(Math.random() * (max - min)) + min;
+  if (number === exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return number;
+  }
+}
+
+const GameScreen = ({ userChoice, onGuess, onGameOver }) => {
+  const [guess, setGuess] = useState(generateRandomNumber(1, 100, userChoice));
+
+  return (
+    <View style={styles.container}>
+      <Title>Opponents Guess</Title>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 28,
+  },
+});
 
 export default GameScreen;
